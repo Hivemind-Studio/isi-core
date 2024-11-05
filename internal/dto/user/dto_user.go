899593ac979ca9dafcf49c43fc6ff7634f548e5e
Juan Daniel <mail.jdnielss@gmail.com>
2024-11-05 10:17:57 +0700

    add: confirmPassword & validate password

--- internal/dto/user/dto_user.go
+++ internal/dto/user/dto_user.go
@@ -1,17 +1,30 @@
 package user
 
+import (
+	"github.com/Hivemind-Studio/isi-core/pkg/httperror"
+	"github.com/gofiber/fiber/v2"
+)
+
 type LoginDTO struct {
 	Email    string `json:"Email"`
 	Password string `json:"password"`
 }
 
-type RegisterDTO struct {
-	Name     string `json:"name" validate:"required"`
-	Password string `json:"password" validate:"required"`
-	Email    string `json:"email" validate:"required"`
+type RegistrationDTO struct {
+	Name            string `json:"name" validate:"required"`
+	Password        string `json:"password" validate:"required,min=4"`
+	ConfirmPassword string `json:"confirmPassword" validate:"required"`
+	Email           string `json:"email" validate:"required"`
 }
 
 type RegisterResponse struct {
 	Name  string `json:"Name"`
 	Email string `json:"email"`
+}
+
+func (p *RegistrationDTO) ValidatePassword() error {
+	if p.Password != p.ConfirmPassword {
+		return httperror.New(fiber.StatusBadRequest, "password and confirm password do not match")
+	}
+	return nil
 }
