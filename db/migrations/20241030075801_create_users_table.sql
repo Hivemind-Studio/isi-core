cf4a63cd00e53dfe166b0169a235e798dd9887be
Juan Daniel <mail.jdnielss@gmail.com>
2024-11-05 10:18:29 +0700

    remove: field username

--- db/migrations/20241030075801_create_users_table.sql
+++ db/migrations/20241030075801_create_users_table.sql
@@ -1,7 +1,6 @@
 -- +goose Up
 CREATE TABLE users (
    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-   username VARCHAR(50) NOT NULL,
    password VARCHAR(255) NOT NULL,
    name VARCHAR(100),
    email VARCHAR(100) NOT NULL,
--- internal/repository/user/repo_user_type.go
+++ internal/repository/user/repo_user_type.go
@@ -6,18 +6,17 @@ import (
 )
 
 type User struct {
-	ID           int64       `db:"id" json:"id"`
-	Username     string      `db:"username" json:"username" validate:"required,min=3,max=50"`
-	Password     string      `db:"password" json:"-"`
-	Name         string      `db:"name" json:"name,omitempty"`
-	Email        string      `db:"email" json:"email" validate:"required,email"`
-	Address      string      `db:"address" json:"address,omitempty"`
-	PhoneNumber  string      `db:"phone_number" json:"phone_number,omitempty"`
-	DateOfBirth  time.Time   `db:"date_of_birth" json:"date_of_birth"`
-	Gender       enum.Gender `db:"gender" json:"gender"`
-	Verification bool        `db:"verification" json:"verification"`
-	Occupation   string      `db:"occupation" json:"occupation"`
-	Status       bool        `db:"status" json:"status"`
-	CreatedAt    time.Time   `db:"created_at" json:"created_at"`
-	UpdatedAt    time.Time   `db:"updated_at" json:"updated_at"`
+	ID           int64        `db:"id" json:"id"`
+	Password     string       `db:"password" json:"-"`
+	Name         string       `db:"name" json:"name,omitempty"`
+	Email        string       `db:"email" json:"email" validate:"required,email"`
+	Address      *string      `db:"address" json:"address,omitempty"`
+	PhoneNumber  *string      `db:"phone_number" json:"phone_number,omitempty"`
+	DateOfBirth  *time.Time   `db:"date_of_birth" json:"date_of_birth"`
+	Gender       *enum.Gender `db:"gender" json:"gender"`
+	Verification *bool        `db:"verification" json:"verification"`
+	Occupation   *string      `db:"occupation" json:"occupation"`
+	Status       *bool        `db:"status" json:"status"`
+	CreatedAt    time.Time    `db:"created_at" json:"created_at"`
+	UpdatedAt    time.Time    `db:"updated_at" json:"updated_at"`
 }
