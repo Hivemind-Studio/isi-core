964bd5499c5c1da92cd0f01713e4fbb79cb84801
Juan Daniel <mail.jdnielss@gmail.com>
2024-11-05 14:02:34 +0700

    fix: duplicate field phone number

--- db/migrations/20241030075802_create_coaches_table.sql
+++ db/migrations/20241030075802_create_coaches_table.sql
@@ -6,7 +6,6 @@ CREATE TABLE coaches (
      experiences TEXT,
      education TEXT,
      rate DECIMAL(10, 2),
-     phone_number VARCHAR(15),
      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
      FOREIGN KEY (user_id) REFERENCES users(id)
