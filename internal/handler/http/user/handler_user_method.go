b13391b16c86983c4d8645e10dc09780d1aca598
Juan Daniel <mail.jdnielss@gmail.com>
2024-11-05 10:17:11 +0700

    add: validation Helper

--- internal/handler/http/user/handler_user_method.go
+++ internal/handler/http/user/handler_user_method.go
@@ -3,23 +3,29 @@ package user
 import (
 	"github.com/Hivemind-Studio/isi-core/internal/dto/user"
 	"github.com/Hivemind-Studio/isi-core/pkg/httphelper/response"
-	"github.com/Hivemind-Studio/isi-core/utils"
+	validatorhelper "github.com/Hivemind-Studio/isi-core/pkg/translator"
 	"github.com/gofiber/fiber/v2"
 )
 
 func (h *Handler) Create(c *fiber.Ctx) error {
-	var newUser user.RegisterDTO
+	var newUser user.RegistrationDTO
 	if err := c.BodyParser(&newUser); err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, "Invalid input")
 	}
 
-	if err := utils.ValidateStruct(newUser); err != nil {
-		return fiber.NewError(fiber.StatusBadRequest, "Validation error")
+	err := newUser.ValidatePassword()
+
+	if err != nil {
+		return err
+	}
+
+	if err := validatorhelper.ValidateStruct(newUser); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
 
 	result, err := h.userService.Create(c, &newUser)
 	if err != nil {
-		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+		return err
 	}
 
 	return c.Status(fiber.StatusCreated).JSON(
