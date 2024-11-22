f1100f98b5645d050dc1674cb64792f7f4cc7533
Juan Daniel <mail.jdnielss@gmail.com>
2024-11-23 01:51:05 +0700

    add: table migration user_verified_account

---
+++ db/migrations/20241122162501_create_verifed_email.sql
@@ -0,0 +1,10 @@
+-- +goose up
+
+CREATE TABLE user_verified_account (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT,
+    verification_token varchar(255),
+    expires_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
