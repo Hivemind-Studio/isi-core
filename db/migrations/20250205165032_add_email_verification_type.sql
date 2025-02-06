d53558da16729fc52b1a4cfe713485254794be8b
Juan Daniel <mail.jdnielss@gmail.com>
2025-02-06 14:33:20 +0700

    remove prefix database

--- db/migrations/20250205165032_add_email_verification_type.sql
+++ db/migrations/20250205165032_add_email_verification_type.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE isi.email_verifications ADD `type` varchar(50) NULL;
+ALTER TABLE email_verifications ADD `type` varchar(50) NULL;
 -- +goose StatementEnd
 
 -- +goose Down
