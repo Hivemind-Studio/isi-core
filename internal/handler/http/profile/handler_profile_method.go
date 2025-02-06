852853553ecb3baec29b3ce112d624531339ce3d
Juan Daniel <mail.jdnielss@gmail.com>
2025-02-06 11:12:49 +0700

    remove unused log

--- internal/handler/http/profile/handler_profile_method.go
+++ internal/handler/http/profile/handler_profile_method.go
@@ -1,7 +1,6 @@
 package profile
 
 import (
-	"fmt"
 	authdto "github.com/Hivemind-Studio/isi-core/internal/dto/auth"
 	dto "github.com/Hivemind-Studio/isi-core/internal/dto/user"
 	"github.com/Hivemind-Studio/isi-core/pkg/httperror"
@@ -18,10 +17,6 @@ func (h *Handler) GetProfile(c *fiber.Ctx) error {
 	if jwtToken == "" || !strings.HasPrefix(jwtToken, "Bearer ") {
 		return fiber.ErrUnauthorized
 	}
-
-	requestId := c.Locals("request_id").(string)
-
-	fmt.Println("REQUEST_ID_CROT ", requestId)
 
 	user, err := middleware.ExtractJWTPayload(jwtToken)
 	if err != nil {
