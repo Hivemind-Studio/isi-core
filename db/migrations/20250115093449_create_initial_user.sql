9d06f5d99d52ab897432910e1654d87ff8368f41
Juan Daniel <mail.jdnielss@gmail.com>
2025-02-06 17:23:44 +0700

    add: default password

--- db/migrations/20250115093449_create_initial_user.sql
+++ db/migrations/20250115093449_create_initial_user.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 INSERT INTO users (id, name, email, password, role_id, phone_number, date_of_birth, gender, occupation)
 VALUES
-    (1, 'Admin', 'admin@isi.com', 'd210de044bf170b9d87eadcb4de927b66aa57302e95e585740c67dbf61a47ae6', 1, '+23456789012', null, null, null),
-    (2, 'Staff', 'staff@isi.com', 'd210de044bf170b9d87eadcb4de927b66aa57302e95e585740c67dbf61a47ae6', 2, '+23456789013', null, null, null),
-    (3, 'Coach', 'coach@isi.com', 'd210de044bf170b9d87eadcb4de927b66aa57302e95e585740c67dbf61a47ae6', 3, '+23456789014', null, null, null),
-    (4, 'Coachee', 'coachee@isi.com', 'd210de044bf170b9d87eadcb4de927b66aa57302e95e585740c67dbf61a47ae6', 4, '+23456789015', null, null, null);
+    (1, 'Admin', 'admin@isi.com', '21f78900fc72b1929a2b12cac3d184c95fad3733621ae370dc1869cc24291057', 1, '+23456789012', null, null, null),
+    (2, 'Staff', 'staff@isi.com', '21f78900fc72b1929a2b12cac3d184c95fad3733621ae370dc1869cc24291057', 2, '+23456789013', null, null, null),
+    (3, 'Coach', 'coach@isi.com', '21f78900fc72b1929a2b12cac3d184c95fad3733621ae370dc1869cc24291057', 3, '+23456789014', null, null, null),
+    (4, 'Coachee', 'coachee@isi.com', '21f78900fc72b1929a2b12cac3d184c95fad3733621ae370dc1869cc24291057', 4, '+23456789015', null, null, null);
 
 INSERT INTO coaches (user_id) VALUES (4);
