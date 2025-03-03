d34be1a9f3d4fbbbc81769d6dfcf2639d420f888
tham <agatha.settham@ajaib.co.id>
2025-03-03 18:00:55 +0700

    add log user login

--- internal/usecase/userlogin/user_login.go
+++ internal/usecase/userlogin/user_login.go
@@ -11,21 +11,28 @@ import (
 )
 
 func (uc *UseCase) Execute(ctx context.Context, body *auth.LoginDTO) (user dto.UserDTO, err error) {
+	requestID := ctx.Value("request_id").(string)
+
+	logger.Print("info", requestID, "user_login", "execute", "attempting login for email", body.Email)
+
 	savedUser, err := uc.repoUser.FindByEmail(ctx, body.Email)
 	if err != nil {
+		logger.Print("error", requestID, "user_login", "execute", "failed to find user", err)
 		return dto.UserDTO{}, err
 	}
 
 	if savedUser.Password == nil {
-		logger.Print("error", ctx.Value("request_id").(string), "user_login", "execute",
-			"user has not set password", body)
+		logger.Print("error", requestID, "user_login", "execute", "user has not set password", body.Email)
 		return dto.UserDTO{}, httperror.New(fiber.StatusBadRequest, "user has not set password")
 	}
 
 	isValidPassword, _ := hash.ComparePassword(body.Password, *savedUser.Password)
 	if !isValidPassword {
+		logger.Print("error", requestID, "user_login", "execute", "invalid password attempt", body.Email)
 		return dto.UserDTO{}, httperror.New(fiber.StatusBadRequest, "invalid password")
 	}
+
+	logger.Print("info", requestID, "user_login", "execute", "login successful", body.Email)
 
 	return dto.UserDTO{
 		ID:    savedUser.ID,
